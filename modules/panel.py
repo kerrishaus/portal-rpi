b5d228fc640321c4fcea2e22e73f5b7702101abb
Kenny McCormick <kennyrmccormick+github@gmail.com>
2021-06-21 16:19:37 -0500

    display message after 10 seconds of inactivity

--- modules/panel.py
+++ modules/panel.py
@@ -3,6 +3,7 @@ import time
 import RPi.GPIO as GPIO
 
 from util import gpio
+from util import timer
 from util.gpio import lights
 from util.gpio import motion
 
@@ -13,11 +14,18 @@ gpio.setup()
 lights.setup()
 motion.setup()
 
+motionTimer = timer.Timer()
+noMotionTimer = timer.Timer()
+
 def update():
 	while True:
 		motionDetected = GPIO.input(motion.MOTION_SENSOR_PIN)
 		if motionDetected:
-			print("Motion Detected!" + str(time.time()))
 			lights.send_light()
-		time.sleep(.5)
+			noMotionTimer.reset()
+		else:
+			motionTimer.reset()
+			if noMotionTimer.getElapsedTime() > 10:
+				print("no motion in 10 seconds")
+		time.sleep(1)
 	return
