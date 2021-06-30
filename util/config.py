fbbb891a667c01b28b579a97e0943838223fa965
Kenny McCormick <kennyrmccormick+github@gmail.com>
2021-06-30 11:33:35 -0500

    create default config if it does not already exist

--- util/config.py
+++ util/config.py
@@ -1,22 +1,36 @@
 import configparser
+import os
 
 configLocation = "/home/pi/portal-rpi/config.ini"
-
 config = configparser.ConfigParser()
-config.read(configLocation)
 
-my_name = config['DEFAULT']['MyName']
-my_addr = config['DEFAULT']['MyAddr']
-my_port = int(config['DEFAULT']['MyPort'])
-my_purpose = config['DEFAULT']['MyPurpose']
+def write_file():
+	config.write(open(configLocation, 'w'))
 
-api_token = config['KUNINDUSTRIES_API']['Token']
-api_status_interval = float(config['KUNINDUSTRIES_API']['StatusInterval'])
-api_auto_update_addr_and_port = config['KUNINDUSTRIES_API']['AutoUpdateAddressAndPort']
-api_device_id = config['KUNINDUSTRIES_API']['DeviceID']
+if not os.path.exists(configLocation):
+	config['DEFAULT']['MyName'] = "Raspberry Pi"
+	config['DEFAULT']['MyAddr'] = "0.0.0.0"
+	config['DEFAULT']['MyPort'] = "27000"
+	config['DEFAULT']['MyPurpose'] = "Develop"
+
+	config['KUNINDUSTRIES_API']['Token'] = "NO-TOKEN"
+	config['KUNINDUSTRIES_API']['StatusInterval'] = 60
+	config['KUNINDUSTRIES_API']['AutoUpdateAddressAndPort'] = True
+	config['KUNINDUSTRIES_API']['DeviceID'] = 0
+else:
+	config.read(configLocation)
+
+	my_name = config['DEFAULT']['MyName']
+	my_addr = config['DEFAULT']['MyAddr']
+	my_port = int(config['DEFAULT']['MyPort'])
+	my_purpose = config['DEFAULT']['MyPurpose']
+
+	api_token = config['KUNINDUSTRIES_API']['Token']
+	api_status_interval = float(config['KUNINDUSTRIES_API']['StatusInterval'])
+	api_auto_update_addr_and_port = config['KUNINDUSTRIES_API']['AutoUpdateAddressAndPort']
+	api_device_id = config['KUNINDUSTRIES_API']['DeviceID']
 
 def updateConfig(section, key, value):
 	config[section][key] = value
 
-	with open(configLocation, 'w') as configfile:
-	    config.write(configfile)
+	write_file()
