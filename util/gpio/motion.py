7d50b456ea94f049f94a8fe1411babda93eb0143
Kenny McCormick <kennyrmccormick+github@gmail.com>
2021-06-30 02:44:52 -0500

    notify api of motion status change

--- util/gpio/motion.py
+++ util/gpio/motion.py
@@ -4,6 +4,7 @@ import subprocess
 import RPi.GPIO as GPIO
 
 from util import timer
+from util import config
 from net import kunapi
 
 MOTION_SENSOR_PIN = 23
@@ -18,11 +19,14 @@ def update():
         if not motion:
             motion = True
             run('vcgencmd display_power 1', shell=True)
+            kunapi.status(3)
     else:
         if motion:
             motion = False
+            kunapi.status(4)
             # dont know if i want to do this just yet
             #run('vcgencmd display_power 0', shell=True)
 
     time.sleep(3)
     return
+    
