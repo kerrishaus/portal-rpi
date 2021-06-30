64cd5a897aed39bbecc4ebe9ee272829ef16cf26
Kenny McCormick <kennyrmccormick+github@gmail.com>
2021-06-30 11:20:37 -0500

    service type should be idle

--- create_service.sh
+++ create_service.sh
@@ -5,7 +5,7 @@ echo "Descrpition=Portal Service" >> portal.service
 echo "After=mutli-user.target" >> portal.service
 echo "" >> portal.service
 echo "[Service]" >> portal.service
-echo "Type=simple" >> portal.service
+echo "Type=idle" >> portal.service
 echo "Restart=always" >> portal.service
 echo "ExecStart=/usr/bin/python3 /home/pi/portal-rpi/portal.py" >> portal.service
 echo "" >> portal.service
