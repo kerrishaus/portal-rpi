f5b44dd369f2cec620d8d668131a45dc98c7a47e
Kenny McCormick <kennyrmccormick+github@gmail.com>
2021-06-30 11:13:00 -0500

    add create_service.sh

---
+++ create_service.sh
@@ -0,0 +1,16 @@
+touch /etc/systemd/system/portal.service
+cd /etc/systemd/system/
+echo "[Unit]" > portal.service
+echo "Descrpition=Portal Service" > portal.service
+echo "After=mutli-user.target" > portal.service
+echo "" > portal.service
+echo "[Service]" > portal.service
+echo "Type=simple" > portal.service
+echo "Restart=always" > portal.service
+echo "ExecStart=/usr/bin/python3 /home/pi/portal-rpi/portal.py" > portal.service
+echo "" > portal.service
+echo "[Install]" > portal.service
+echo "WantedBy=multi-user.target" > portal.service
+systemctl daemon-reload
+systemctl enable portal.service
+systemctl start portal.service
