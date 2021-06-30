6e4d72e738233c5bb854dc87e065096d7583db28
Kenny McCormick <kennyrmccormick+github@gmail.com>
2021-06-30 02:13:09 -0500

    fix api_auto_update_addr_and_port being misnamed

--- util/config.py
+++ util/config.py
@@ -9,7 +9,7 @@ my_name = config['DEFAULT']['MyName']
 my_purpose = config['DEFAULT']['MyPurpose']
 api_token = config['KUNINDUSTRIES_API']['Token']
 api_status_interval = config['KUNINDUSTRIES_API']['StatusInterval']
-api_status_interval = config['KUNINDUSTRIES_API']['AutoUpdateAddressAndPort']
+api_auto_update_addr_and_port = config['KUNINDUSTRIES_API']['AutoUpdateAddressAndPort']
 api_device_id = config['KUNINDUSTRIES_API']['DeviceID']
 
 def updateConfig(section, key, value):
