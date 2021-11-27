cf69cecc2a929aed1e7952b6d944e2b5a22170b8
Kenny McCormick <kennyrmccormick+github@gmail.com>
2021-11-27 12:27:09 -0600

    fix idletime returning empty char for output and attempting to cast it to int

--- util/display.py
+++ util/display.py
@@ -23,5 +23,9 @@ def get_idle_time():
     result = subprocess.run('export DISPLAY=:0 && sudo -u pi xprintidle', shell=True, capture_output=True)
     output = str(result.stdout)
     output = output[2:len(output) - 3]
+
+    if output == '':
+        return -1
+
     idletime = int(output)
     return idletime
