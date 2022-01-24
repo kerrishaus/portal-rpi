e7f53859360545688315255a27f012026842f1b6
Kenny McCormick <kennyrkun@users.noreply.github.com>
2022-01-23 21:28:05 -0600

    Update radar.py

--- util/gpio/motion/radar.py
+++ util/gpio/motion/radar.py
@@ -1,5 +1,6 @@
 
 import RPi.GPIO as GPIO
+from gpiozero import DigitalInputDevice
 
 import subprocess
 
@@ -8,31 +9,42 @@ from util import config
 from util import display
 from net import kerrishausapi
 
-MOTION_SENSOR_PIN = 14
+MOTION_SENSOR_PIN = 17
 
 motion = False
+motion_radar = DigitalInputDevice(MOTION_SENSOR_PIN, pull_up=False)
 
 def setup():
     GPIO.setup(MOTION_SENSOR_PIN, GPIO.IN)
 
+def motion_start():
+    global motion
+    
+    if not motion
+        motion = True
+        print("motion detected")
+        kerrishausapi.status(4)
+        
+        subprocess.run("export DISPLAY=:0 && sudo -u pi xset s reset", shell=True, capture_output=True)
+        
+        if not display.is_display_powered():
+            display.display_power_on()
+
+def motion_stop():
+    global motion
+    
+    if motion:
+        motion = False
+        print("motion no longer detected")
+    
 def update():
     global motion
+    
+    radar.when_activated = motion_start
+    radar.when_deactivated = motion_stop
 
-    if GPIO.input(MOTION_SENSOR_PIN):
-        if not motion:
-            motion = True
-            print("motion detected")
-            kerrishausapi.status(4)
+    if not motion and display.is_display_powered():
+        if display.get_idle_time() > (config.screen_idle_time * 1000):
+            display.display_power_off()
 
-            subprocess.run('export DISPLAY=:0 && sudo -u pi xset s reset', shell=True, capture_output=True)
-
-            if not display.is_display_powered():
-                display.display_power_on()
-    else:
-        if motion:
-            motion = False
-            print("motion no longer detected")
-        elif not motion and display.is_display_powered():
-            if display.get_idle_time() > (config.screen_idle_time * 1000):
-                display.display_power_off()
     return
