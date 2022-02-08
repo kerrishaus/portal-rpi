76830a3f9ef4d1fc7249cf63e979437bc4ca42ee
Kenny McCormick <kennyrkun@users.noreply.github.com>
2022-02-07 20:12:50 -0600

    Update passiveir.py

--- util/gpio/motion/passiveir.py
+++ util/gpio/motion/passiveir.py
@@ -19,6 +19,8 @@ def update():
     global motion
 
     if GPIO.input(MOTION_SENSOR_PIN):
+        print("motion pin high")
+        
         subprocess.run('export DISPLAY=:0 && sudo -u pi xset s reset', shell=True, capture_output=True)
         
         if not motion:
